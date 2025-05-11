3d3a1fe55e8f37c92d0ad7ea7625d22705cc03b8
dakku <116499879+dakku7@users.noreply.github.com>
2025-05-11 17:53:46 +0200

    Add files via upload

---
+++ деконструктор/деконструктор.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class myClass {
+	int a, b;
+	int name;
+public:
+
+	myClass(int data) {
+		a = 0;
+		b = 0;
+		name = data;
+		cout << "constructor " << name << " is up!" << endl;
+	}
+	
+	void show() {
+		cout << "A: " << a << endl;
+		cout << "B: " << b << endl;
+	}
+
+	~myClass() {
+
+
+		cout << "Deconstructor " << name << " is up!" << endl;
+	}
+	
+};
+
+void Foo() {
+	cout << "Foo is started" << endl;
+
+	myClass a(1);
+	myClass b(2);
+
+	cout << "Foo is ended" << endl;
+}
+
+int main() {
+
+	Foo();
+
+	return 0;
+}
